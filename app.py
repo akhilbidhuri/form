00bf14643dbf3488e0f33c40686ef439cdd4553b
akhilbidhuri <akhilbidhuri@gmail.com>
2017-11-16 22:20:13 +0530

    yo

--- app.py
+++ app.py
@@ -1,18 +1,62 @@
-from flask import Flask
-from datetime import datetime
-app = Flask(__name__)
+from flask import Flask,render_template,request,redirect
+import csv
 
-@app.route('/')
-def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+app=Flask("__name__")
 
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
+a = 0
 
-    <img src="http://loremflickr.com/600/400" />
-    """.format(time=the_time)
+@app.route("/",methods=['POST','GET'])
+def goto():
+    return render_template("form.html")
 
-if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+@app.route("/submit",methods=['POST','GET'])
+def submit():
+    global a
+    a = a + 1
+    row = list()
+    
+#    return redirect("https://thunderrlogistics.com/index1.html")
+#    fname = request.form["first_name"]
+#    lname = request.form["last_name"]
+#    email = request.form["email"]
+#
+#    pno = request.form["phone"]
+#    add = request.form["address"]
+#    zipc = request.form["zip"]
+#    age = request.form["age"]
+#    bg = request.form["Bloodg"]
+#    fullt = request.form["full-time"]
+#    emp = request.form["employed-y"]
+#    lisc = request.form["license-y"]
+#    bnk = request.form["bank-y"]
+#    cmp = request.form["comp-y"]
+#    smtph = request.form["smartphone-y"]
+#    sal = request.form["Salary"]
+#    lang = request.form["languages"]
+    row=[   a,  request.form["first_name"]
+    , request.form["last_name"]
+    , request.form["email"]
+    , request.form["phone"]
+    , request.form["address"]
+    , request.form["zip"]
+    , request.form["age"]
+    , request.form["Bloodg"]
+    , request.form["full-time"]
+    , request.form["employed-y"]
+    , request.form["license-y"]
+    , request.form["bank-y"]
+    , request.form["comp-y"]
+    , request.form["smartphone-y"]
+    , request.form["Salary"]
+    , request.form["languages"]
+    ]
+    with open("log_data.csv","w+") as f:
+        writer = csv.writer(f)
+        writer.writerow(row)
+        
+    return redirect("https://thunderrlogistics.com/index1.html")
+    
+    
 
+if __name__ == "__main__":
+    app.run()
